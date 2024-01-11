7c18c9b9f8d4c295abb7d0e9b8e10588c77c48cf
Olexiy Sokurenko <sokurenko-extern@gvl.de>
2024-01-11 17:23:48 +0100

    Add screenshot capture for failed tests
    The update adds an AfterStep function which captures a screenshot if a test scenario fails. The screenshot taken by this function is then attached to the relative scenario, adding visual reference for the failure. Timestamp is also included for precise error tracking.

--- src/test/java/de/olexiy/texting/example/cucumberauthomationframework/stepDefenitions/base/Hooks.java
+++ src/test/java/de/olexiy/texting/example/cucumberauthomationframework/stepDefenitions/base/Hooks.java
@@ -1,7 +1,13 @@
 package de.olexiy.texting.example.cucumberauthomationframework.stepDefenitions.base;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.sql.Timestamp;
 
 import static de.olexiy.texting.example.cucumberauthomationframework.driver.DriverFactory.cleanUp;
 import static de.olexiy.texting.example.cucumberauthomationframework.driver.DriverFactory.getDriver;
@@ -11,6 +17,20 @@ public class Hooks {
     public void setup() {
         getDriver();
     }
+
+    @AfterStep
+    public void captureExceptionImage(Scenario scenario) {
+        if (scenario.isFailed()) {
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            String timeMilliseconds = Long.toString(timestamp.getTime());
+
+            byte[] screenshot = ((TakesScreenshot) getDriver())
+                    .getScreenshotAs(OutputType.BYTES);
+
+            scenario.attach(screenshot, "image/png", timeMilliseconds);
+        }
+    }
+
     @After
     public void tearDown() {
         cleanUp();
